3a8b50ea40b223789bc28adf5d58490c993151f8
Caizen <418590624@qq.com>
2022-04-21 23:49:29 +0800

    Update at 0421 night

--- Comprehensive/08-Event-Comphrehensive/advancedBanner/advBanner.js
+++ Comprehensive/08-Event-Comphrehensive/advancedBanner/advBanner.js
@@ -1,9 +1,8 @@
 const moveBar = document.querySelector('.img-box-move')
 const leftArrow = document.querySelector('.left-arrow')
 const rightArrow = document.querySelector('.right-arrow')
-
-
 const lines = document.querySelectorAll('.line')
+
 const linesArr = [...lines]
 
 
@@ -11,26 +10,25 @@ let countIndex = 0
 
 
 function arrowHandle(e){
-
-
-    const isLeft = e.currentTarget.classList.contains('left-arrow')//返回 true / false
-
+    const isLeft = e.currentTarget.classList.contains('left-arrow')
     if (isLeft) {
-
+        //点击的是左箭头
 
     } else {
-        //点了右箭头
+        //那么点击的就是右箭头
+        if(countIndex===0) {//👇切换回第一张,就可以有动画
+            moveBar.style.transition = 'transform 0.5s ease-in-out'
+        }
         countIndex++
         moveBar.style.transform = `translate(-${countIndex * 1024}px)`
     }
-
-
-    
-    // moveBar.style.transform = `translate(-${countIndex * 1024}px)`
+    console.log(countIndex)
     
 }
 
 
-
 leftArrow.addEventListener('click',arrowHandle)
 rightArrow.addEventListener('click',arrowHandle)
+
+
+
