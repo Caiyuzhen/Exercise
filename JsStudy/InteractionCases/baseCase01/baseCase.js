c7ec0d762a97a33267546672eef7f1e9e4791c98
Zen <47984714+Caiyuzhen@users.noreply.github.com>
2022-04-23 18:45:33 +0800

    🚀Update at 0423 afternoon

--- JsStudy/InteractionCases/baseCase01/baseCase.js
+++ JsStudy/InteractionCases/baseCase01/baseCase.js
@@ -9,3 +9,24 @@ console.log('onclick' in window)
 
 //e.traget 永远是真正点击到的元素
 //e.currentTarget 是函数绑定的元素
+
+
+//🌟🌟🌟需要传多个参数的方法
+//多套一层
+const h2 = document.querySelector('.p2')
+const aaa = 111
+const bbb = 222
+h2.addEventListener('click',(e)=>{
+    test(e,aaa,bbb)
+})
+//这个才是具体的函数
+function test(e,x,y){
+    console.log(bbb)
+}
+
+//🌟🌟🌟不多套一层也行,变量可以写在函数内👇
+h2.addEventListener('click',(e,aaa,bbb)=>{
+    const aa = 1
+    const bb = 2
+    console.log(bb)
+})
