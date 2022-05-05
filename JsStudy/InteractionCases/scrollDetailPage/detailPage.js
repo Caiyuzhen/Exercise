785311d8c10f9bedb960bd056539fc1568e4069a
caiyuzhen <caiyuzhen@bytedance.com>
2022-05-05 14:24:04 +0800

    🚀Update at 0505

--- JsStudy/InteractionCases/scrollDetailPage/detailPage.js
+++ JsStudy/InteractionCases/scrollDetailPage/detailPage.js
@@ -3,9 +3,15 @@ const stackArr = [...document.querySelectorAll(".stack-container")]
 
 const deltaBaseY = 50 //因为每一层 Stack 都相差 50px
 
-const targetY = 600 //从 600 px 开始变化
+const targetY = 600 //第最后一个图形的 Y 值, 往后都是 targetY - index * 50px
 
 const delatTotalY = 0 //滚动的总值
+
+
+const transform = getComputedStyle(stackArr[0]).transform
+const matrix = new DOMMatrixReadOnly(stackArr[0])
+const baseTranslateY = matrix.m42 
+
 
 
 function stackLayerMove(layer,index,targetY) { 
