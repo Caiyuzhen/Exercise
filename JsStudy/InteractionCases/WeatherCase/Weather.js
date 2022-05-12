1ba5a19918d967c74d627b65892470eee18d75a4
Zen <47984714+Caiyuzhen@users.noreply.github.com>
2022-05-13 00:42:07 +0800

    🚀Update at 0513 morning

--- JsStudy/InteractionCases/WeatherCase/Weather.js
+++ JsStudy/InteractionCases/WeatherCase/Weather.js
@@ -27,24 +27,6 @@ cloudyIconBox.addEventListener('mouseenter',()=>{
 
 
 
-
-
-//🌟🌟动画完成后，还原为最初的形态！
-sunIcon.addEventListener('transitionend',(e)=>{ //🌟🌟🌟旋转动画完成后，重置数据！！
-    
-    //重置太阳移动效果
-    e.currentTarget.style.transition = 'none'; //🌟🌟🌟让它不会一直动！！配合下面的重置一起使用！！
-    e.currentTarget.classList.remove('sun-rotate')
-    
-    setTimeout(()=>{ //🌟🌟🌟，重置动画效果！动完后又能动！！
-        e.target.style.transition = `transform 1.65s ease-in-out` 
-    })
-
-})
-
-
-
-
 cloudyIconBox.addEventListener('mouseleave',()=>{
     //鼠标移开时,让两个锁变为 true
     cloudyIconBigLock = true
@@ -53,19 +35,107 @@ cloudyIconBox.addEventListener('mouseleave',()=>{
 
 
 
-//🌟🌟动画运行时，让两个锁
+
+//🌟🌟动画完成后，还原为最初的形态！
+sunIcon.addEventListener('transitionend',(e)=>{ //🌟🌟🌟旋转动画完成后，重置数据！！
+    
+    // //重置太阳移动效果
+    e.currentTarget.style.transition = 'none' //🍔一： （先移除动画）
+    e.currentTarget.classList.remove('sun-rotate') //🍔二：（再移动移动的属性）
+    
+    setTimeout(()=>{ //🍔三： （最后加回动画！）
+        e.target.style.transition = `transform 1.65s ease-in-out` 
+
+    })
+})
+
+
+
+
+//🌟🌟动画运行时，让两个云朵不能移动！
 cloudyIconBigCloud.addEventListener('animationiteration',(e)=>{ //🌟🌟🌟很关键，animationiteration 表示在动画执行的过程中云朵都会动
-    while(cloudyIconBigLock){ 
+    if(cloudyIconBigLock){ 
         // e.target 是利用子元素的冒泡来获取元素(指向的是谁🌟触发了这个事件流-目标元素)
         //e.currentTarget 是获取真实被绑定事件的元素
         e.currentTarget.classList.remove('icon-cloudy-big-cloud-ani')
     } 
-    while(cloudyIconSmallLock){
+     
+})
+
+cloudyIconBigCloud.addEventListener('animationiteration',(e)=>{ //🌟🌟🌟很关键，animationiteration 表示在动画执行的过程中云朵都会动
+
+    if(cloudyIconSmallLock){
         e.currentTarget.classList.remove('icon-cloudy-small-cloud-ani')
     }
      
 })
 
+
+
+// ————————————————————————————————————————————————————————————————————
+
+
+const IconSunnyBox = document.querySelector('.icon-box-sunny')
+const sunnySmallIcon = document.querySelector('.smaller')
+const sunnyBigIcon = document.querySelector('.bigger')
+
+
+let smallerLock = false
+let biggerLock = false
+
+
+
+//太阳飞入+飞走
+IconSunnyBox.addEventListener('mouseenter', (e) =>{
+
+    if(!smallerLock && !biggerLock){ //必须得下面两个都执行完重置了才会执行这个动画！！
+
+    smallerLock = true
+    biggerLock = true
+
+    sunnySmallIcon.classList.add('fly-out')
+    sunnyBigIcon.classList.add('fly-in')
+
+    }
+    
+})
+
+
+
+
+//🌟🌟动画完成后，还原为最初的形态！
+sunnyBigIcon.addEventListener('transitionend',(e)=>{ 
+
+
+    biggerLock = false
+
+    e.currentTarget.style.transition = 'none' //🍔一（先不要动画）：不加这个太阳就会过渡的飞回去！！！加了后就 0 秒就飞回去了！！
+    e.currentTarget.classList.remove('fly-in')//🍔二（再移除飞入的效果）
+
+    const target = e.currentTarget
+    setTimeout(()=>{ //🍔三（最后加回动画效果！！）
+        target.style.transition = 'transform 1.5s ease-in-out'
+    })
+
+
+})
+
+
+sunnySmallIcon.addEventListener('transitionend',(e)=>{   //太阳飞走后，重置归位！！
+
+
+    smallerLock = false
+
+
+    e.currentTarget.style.transition = 'none' //🍔一（先不要动画）：不加这个太阳就会过渡的飞回去！！！加了后就 0 秒就飞回去了！！
+    e.currentTarget.classList.remove('fly-out')//🍔二（再移除飞入的效果）
+
+    const target = e.currentTarget
+    setTimeout(()=>{ //🍔三（最后加回动画效果！！）
+        target.style.transition = 'transform 1.5s ease-in-out'
+    })
+
+})
 
 
 
