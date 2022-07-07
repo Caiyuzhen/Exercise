0c4f21de8152b7c7dd11f93caf0861ba548fcbf7
yuzhen <caiyuzhen00@gmail.com>
2022-07-07 16:12:59 +0800

    Update at 0707

--- JsStudy/mobileDev/mobileCard/mobileCard.js
+++ JsStudy/mobileDev/mobileCard/mobileCard.js
@@ -20,20 +20,39 @@ class Circle {
 
 //Page 类
 class Page {
-	//静态属性
-	static pageTarget = document.querySelector('.pages-box-container')
+	//静态属性,先获得【页面容器 pages-box-container】
+	static pageTemplate = document.querySelector('.pages-box-container')
 
-	//初始化
+	//初始化, 通过 pageData 来穿参（页面内容）
 	constructor(pageData) {
-		this.dom = Page.pageTarget.cloneNode(true) //克隆 card
-		this.initPageContent(pageData)	//在初始化时执行动态生成 page 内容的方法
+		this.dom1 = Page.pageTemplate.cloneNode(true) //克隆 card
+		this.initPageContent(pageData)	//执行注入 page 内容的方法
 	}
 
-	initPageContent(pageData) {
-		const {texts, color, detailText ,imgUrl} = pageData
 
-		//修改大标题
-		const spanArr = [...this.dom.querySelector('.pages-box-container')]
+	//定义静态方法，注入页面内容
+	initPageContent(pageData) {
+		
+		//从 pageData 内解构出  texts, color, detailText ,imgUrl 数据
+		const {texts, color, detailText, imgUrl} = pageData
+		
+		//修改每个卡片的大标题(因为预先在 span 内存了 title 的数据，所以可以遍历出来)
+		const spanArr = [...this.dom1.querySelector('.pages-box-container').children]
+		spanArr.forEach((span,index) => {//修改原数组的每一项，用 forEach
+			span.innerText = texts[index]
+		})
+		
+		//修改每个卡片的背景色
+		const cardBoxBg = this.dom1.querySelector('.page-card')
+		cardBoxBg.style.backgroundColor = color
+		
+		//修改每个卡片的文字
+		const cardBoxText = this.dom1.querySelector('p')
+		cardBoxText.innerText = detailText
+		
+		//修改每个卡片的图片
+		const cardBoxImg = this.dom1.querySelector('img')
+		cardBoxImg.url = imgUrl
 	}
 
 
