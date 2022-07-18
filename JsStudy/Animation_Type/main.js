8b189640b9263a8f1a1ec06be762f20c061b8e43
yuzhen <caiyuzhen00@gmail.com>
2022-07-18 13:25:19 +0800

    fix

--- JsStudy/Animation_Type/main.js
+++ JsStudy/Animation_Type/main.js
@@ -4,12 +4,20 @@
 			将返回一个 Animation 的实例
 			 	🌟调用 pause() 来停止初始化的动画
 				🌟调用 play() 来开始动画
-				🌟调用 playState() 来判断状态
+				🌟调用 playState来判断状态
+				🌟调用cancel()来取消动画
+					里边包含几个重要值
+						idle        当前动画事件无法解析
+						pending     动画将一直等到某些等待中的任务完成才会执行
+						running     动画正在运行
+						paused      动画已经中止
+						finished   动画达到某一临界点
+					
 
 		相当于css keyframe 帧动画的 js 版本
 
 
-		过往的方式：
+		几种添加动画的方式：
 			方式一：添加 transform 属性
 			方式二：添加包含 kf 动画的类
 			方式三：JS 添加 animate() 动画
@@ -18,7 +26,7 @@
 const BOX = document.querySelector('.box');
 
 
- //要传入两个数据，一个是数组, 里边包含很多对象, 类似帧动画的写法
+//要传入两个数据，一个是数组, 里边包含很多对象, 类似帧动画的写法
 const BOX_ANMI = BOX.animate([
 	{
 		transform: 'translateX(0px)',
@@ -32,5 +40,13 @@ const BOX_ANMI = BOX.animate([
 BOX_ANMI.pause() //一开始先暂停动画
 
 BOX.addEventListener('click',()=>{
-	BOX_ANMI.play() //点击元素之后再播放动画
+	// BOX_ANMI.play() //点击元素之后再播放动画
+	// console.log(BOX_ANMI.playState)
+	
+	//🔥🔥点击元素时, 判断是不是暂停的状态, 是的话就让它开始运动, 否则就让它停下来
+	if(BOX_ANMI.playState === 'paused'){ 
+		BOX_ANMI.play()
+	}else{
+		BOX_ANMI.pause()
+	}
 })
