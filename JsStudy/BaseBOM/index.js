b17ae21165013bcad4bb1584bf0c5284e74b4b2f
yuzhen <caiyuzhen00@gmail.com>
2023-02-03 14:00:34 +0800

    update

---
+++ JsStudy/BaseBOM/index.js
@@ -0,0 +1,8 @@
+// window.devicePixelRatio 输出屏幕的像素比
+
+// const result = confirm('确定要删除吗？') //🌟用来判断用户是否点击了确定按钮
+// console.log(result) // true or false
+
+//const result = prompt('请输入你的名字') //🌟用来获取用户输入的内容
+//console.log(result) // 用户输入的内容
+
