46d9dc977523994eb01770112ce0f38288f56502
ByteDance <caiyuzhen00@gmail.com>
2023-10-20 13:46:22 +0800

    base

---
+++ database/server.js
@@ -0,0 +1,19 @@
+/*
+	数据库类型
+		关系型数据库 RDBMS => 使用表格来存储数据
+			MySQL
+			PostgreSQL
+			Oracle
+			Microsoft SQL Server
+		非关系型数据库 => 使用键值对、文档、图形等方式来存储数据
+			MongoDB
+			Redis
+			Cassandra
+			Elasticsearch
+
+	CMS
+		无头 CMS => 可以跟任意一个网站结合
+			Strapi
+				npx create-strapi-app@latest my-project --quickstart
+		有头 CMS => 专属于某个网站
+*/
